9e56636e2a03544d933667d9d8e5b53e5fb34b9a
Mario Lucki <luckimario@gmail.com>
2019-07-01 09:59:37 +0200

    Clear up login layout

--- src/views/auth/LoginView.js
+++ src/views/auth/LoginView.js
@@ -20,22 +20,20 @@ const LoginView = ({ isKeyboardUp, navigation }) => (
     <Card style={{ zIndex: 999, flex: 1 }}>
       <TabNavigator navigation={navigation} />
       <FieldGap gap={24} />
-      <TextField placeholder="Username" />
-      <FieldGap gap={16} />
-      <TextField placeholder="Email Address" />
+      <TextField placeholder="Username or Email" />
       <FieldGap gap={16} />
       <TextField placeholder="Password" />
       <FieldGap gap={16} />
-      <TextField editable={false} placeholder="Sign in with Facebook" />
+      <TextField editable={false} placeholder="Login in with Facebook" />
       <FieldGap gap={16} />
-      <TextField editable={false} placeholder="Sign in with Google" />
+      <TextField editable={false} placeholder="Login in with Google" />
       <FieldGap gap={34} />
       <CenteredContent>
         <Label upperCased={false}>
-        By pressing “submit” you agree to our
+        Don’t have an account?
         </Label>
-        <Link onPress={() => console.warn('Open terms and conditions')}>
-        terms & conditions
+        <Link onPress={() => console.warn('Open signup')}>
+        Sign up
         </Link>
         <FieldGap gap={25} />
       </CenteredContent>
